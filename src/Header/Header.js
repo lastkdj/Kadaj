e57cf98d4304537521d4d0afb603b97d1a069dc3
Kadaj <64706671+lastkdj@users.noreply.github.com>
2021-04-03 13:48:36 -0300

    NavBar sticky

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,43 +1,106 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import useStyles from "./Styles/Header";
 import Button from "@material-ui/core/Button";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
   const classes = useStyles();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 70;
+
+      if (isTop === false) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    });
+    const proxyEmpty = () => {};
+    return () => {
+      window.removeEventListener("scroll", proxyEmpty);
+    };
+  }, []);
 
   const onClick = () => {
     window.open("https://www.youtube.com/user/darkkadaj11");
   };
 
   return (
-    <Grid container xs={12} className={classes.background}>
+    <Grid
+      container
+      xs={12}
+      className={scrolled ? classes.sticky : classes.background}
+    >
       <Grid container item xs={12} style={{ margin: "0px 450px" }}>
         <Grid container item xs={3} className={classes.menulist}>
           <Grid container item xs={2}>
-            <Typography className={classes.navbarLogo}>KADAJ</Typography>
+            <Typography
+              className={classes.navbarLogo}
+              style={{ color: scrolled ? "black" : "white" }}
+            >
+              KADAJ
+            </Typography>
           </Grid>
         </Grid>
         <Grid container item xs={9} style={{ justifyContent: "flex-end" }}>
           <ul className={classes.ul}>
             <li className={classes.li}>
-              <Typography className={classes.headerfont}>Specs</Typography>
+              <Typography
+                className={
+                  scrolled ? classes.headerfontSticky : classes.headerfont
+                }
+              >
+                Specs
+              </Typography>
             </li>
             <li className={classes.li}>
-              <Typography className={classes.headerfont}>Details</Typography>
+              <Typography
+                className={
+                  scrolled ? classes.headerfontSticky : classes.headerfont
+                }
+              >
+                Details
+              </Typography>
             </li>
             <li className={classes.li}>
-              <Typography className={classes.headerfont}>Contact</Typography>
+              <Typography
+                className={
+                  scrolled ? classes.headerfontSticky : classes.headerfont
+                }
+              >
+                Contact
+              </Typography>
             </li>
             <li className={classes.li}>
-              <Typography className={classes.headerfont}>FAQ</Typography>
+              <Typography
+                className={
+                  scrolled ? classes.headerfontSticky : classes.headerfont
+                }
+              >
+                FAQ
+              </Typography>
             </li>
             <li className={classes.li}>
-              <Typography className={classes.headerfont}>Other</Typography>
+              <Typography
+                className={
+                  scrolled ? classes.headerfontSticky : classes.headerfont
+                }
+              >
+                Other
+              </Typography>
             </li>
             <li className={classes.li}>
-              <Typography className={classes.headerfont}>Info</Typography>
+              <Typography
+                className={
+                  scrolled ? classes.headerfontSticky : classes.headerfont
+                }
+              >
+                Info
+              </Typography>
             </li>
           </ul>
           <Grid item>
--- src/Header/Styles/Header.js
+++ src/Header/Styles/Header.js
@@ -4,12 +4,32 @@ const useStyles = makeStyles((theme) => ({
   background: {
     padding: "20px 0px",
     opacity: "0.9",
+    transition: "0.6s",
+  },
+  sticky: {
+    display: "flex",
+    padding: "20px 0px",
+    backgroundColor: "white",
+    position: "sticky",
+    top: "0",
+    zIndex: "1",
+    transition: "0.6s",
+    height: "60px",
+    alignContent: "center",
   },
 
   headerfont: {
     margin: "0px 20px",
     display: "flex",
     color: "white",
+    fontFamily: "Poppins, sans-serif",
+    justifyContent: "center",
+  },
+
+  headerfontSticky: {
+    margin: "0px 20px",
+    display: "flex",
+    color: "black",
     fontFamily: "Poppins, sans-serif",
     justifyContent: "center",
   },
